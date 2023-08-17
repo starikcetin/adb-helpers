34596928d8bc7d89fad8976b4b2e340a345f2956
starikcetin <cetinsamedtarik@gmail.com>
2023-08-17 04:14:04 +0300

    install-abb allow keystore

--- install-abb.sh
+++ install-abb.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
 echo "$(basename $0)"
-
 SCRIPT_PATH=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-BUNDLETOOL_PATH="${SCRIPT_PATH}/tools/bundletool.sh"
 
 ABB_PATH=""
+KEYSTORE_PATH=""
+KEYSTORE_PASS=""
+KEY_ALIAS=""
+KEY_PASS=""
 
 printUsage() {
-    echo "Usage: install-abb -a <ABB_PATH>"
-    echo "  -a ABB_PATH: Path to the ABB file. Required."
+    echo "Usage: install-abb --bundle <ABB_PATH> [--ks <KEYSTORE_PATH> --ks-pass <KEYSTORE_PASS> --ks-key-alias <KEY_ALIAS> --key-pass <KEY_PASS>]"
+    echo "  --bundle ABB_PATH: Path to the ABB file. Required."
+    echo "  --ks KEYSTORE_PATH: Path to the ABB file."
+    echo "  --ks-pass KEYSTORE_PASS: Path to the ABB file."
+    echo "  --ks-key-alias KEY_ALIAS: Path to the ABB file."
+    echo "  --key-pass KEY_PASS: Path to the ABB file."
 }
 
-while getopts "a:b:" OPT; do
-    case "${OPT}" in
-        a)
-            ABB_PATH="${OPTARG}"
-        ;;
+while [ "${1:-}" != "" ]; do
+    case "${1}" in
+        "--bundle") shift; ABB_PATH="${1}";;
+        "--ks") shift; KEYSTORE_PATH="${1}";;
+        "--ks-pass") shift; KEYSTORE_PASS="${1}";;
+        "--ks-key-alias") shift; KEY_ALIAS="${1}";;
+        "--key-pass") shift; KEY_PASS="${1}";;
         *)
-			echo "Unrecognized opt: ${OPT}"
+            echo "Unrecognized opt: ${1}"
             printUsage
             exit 1
         ;;
     esac
+    shift
 done
 
 if [[ -z "${ABB_PATH}" ]] ; then
@@ -31,10 +40,27 @@ if [[ -z "${ABB_PATH}" ]] ; then
     exit 1
 fi
 
+if (( (0 ${KEYSTORE_PATH:++1} ${KEYSTORE_PASS:++1} ${KEY_ALIAS:++1} ${KEY_PASS:++1}) % 4 != 0 ))
+then
+    echo "You need to either define all opts regarding the keystore, or none of them."
+    printUsage
+    exit 1
+fi
+
+BUNDLETOOL_PATH="${SCRIPT_PATH}/tools/bundletool.sh"
 TEMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 APKS_PATH="${TEMP_DIR}/x.apks"
 
-"${BUNDLETOOL_PATH}" build-apks --bundle="${ABB_PATH}" --output="${APKS_PATH}"
+if [ -z "${KEYSTORE_PATH}" ] ; then 
+    "${BUNDLETOOL_PATH}" build-apks --bundle="${ABB_PATH}" --output="${APKS_PATH}"
+else
+    "${BUNDLETOOL_PATH}" build-apks --bundle="${ABB_PATH}" --output="${APKS_PATH}" \
+    --ks="${KEYSTORE_PATH}" \
+    --ks-pass="pass:${KEYSTORE_PASS}" \
+    --ks-key-alias="${KEY_ALIAS}" \
+    --key-pass="pass:${KEY_PASS}"
+fi
+
 "${BUNDLETOOL_PATH}" install-apks --apks="${APKS_PATH}"
 
 rm -rf "${TEMP_DIR}"
